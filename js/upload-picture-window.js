e27a4cd598cacf2e799c966b5c69f05aaa48b3b5
ilianameit <ilianameit@gmail.com>
2022-11-21 23:17:25 +0300

    .

--- js/upload-picture-window.js
+++ js/upload-picture-window.js
@@ -1,26 +1,22 @@
 import {ModalWindow} from './modal-window.js';
-import {isEscapeKey} from './util.js';
-import {standartImg} from'./filters.js';
+import {standartImg} from'./filters-for-upload-image.js';
+import './upload-image.js';
 import {inputHashtag, inputDescription } from './pristine-image-form.js';
 const buttonFile = document.querySelector('.img-upload__control');
 const windowDownload = document.querySelector('.img-upload__overlay');
 const cancelForm = document.querySelector('#upload-cancel');
-const modalDownloadWindow = new ModalWindow(windowDownload);
+const modalLoadWindow = new ModalWindow(windowDownload);
 
 buttonFile.addEventListener('click', () => {
   standartImg();
-  modalDownloadWindow.openPopup();
+  modalLoadWindow.openPopup();
 });
 cancelForm.addEventListener('click', () => {
-  modalDownloadWindow.closePopup();
+  modalLoadWindow.closePopup();
   standartImg();
 });
-document.addEventListener('keydown', (evt) => {
-  if(isEscapeKey(evt)) {
-    standartImg();
-  }
-});
 
-modalDownloadWindow.removeEsc(inputHashtag);
-modalDownloadWindow.removeEsc(inputDescription);
-export {modalDownloadWindow};
+modalLoadWindow.removeEsc(inputHashtag);
+modalLoadWindow.removeEsc(inputDescription);
+
+export {modalLoadWindow};
