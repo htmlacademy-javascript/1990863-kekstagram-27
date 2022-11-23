fbb32a587c7fe34162481e0ccd4f54ed3550c0b9
ilianameit <ilianameit@gmail.com>
2022-11-23 18:57:50 +0300

    лишние пробелы удалены

--- js/util.js
+++ js/util.js
@@ -40,14 +40,6 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-
-const debounce = (callback, timeoutDelay = 500) => {
-  let timeoutId;
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-  };
-};
 const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
   return (...rest) => {
